e073b1816c396392b9c971e63f6ef8532ef01663
JohnGUnderwood <4124456+JohnGUnderwood@users.noreply.github.com>
2023-10-09 17:34:00 +0100

    minor tweak

--- tunafish/components/app-tutorial/search-bar.js
+++ tunafish/components/app-tutorial/search-bar.js
@@ -3,7 +3,7 @@ import { SearchInput } from "@leafygreen-ui/search-input"
 function SearchBar(){
     return (
         <div style={{paddingTop:"10px", display:"flex", justifyContent:"center"}}>
-            <SearchInput onChange="" aria-label="some label"></SearchInput>
+            <div style={{width:"70%"}}><SearchInput onChange="" aria-label="some label"></SearchInput></div>
         </div>
     )
 }
